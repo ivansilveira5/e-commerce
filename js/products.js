6520aa73d2a038932e2d6eba4bcc9ffe4db9026f
Pablo Ribeiro <pabloribeiro2302@gmail.com>
2023-08-14 10:00:49 -0300

    Commit de productos
    El trabajo realizado:
    * Fetch de datos y creación de elementos con JS
    * Hoja de estilos particular
    * Eliminación de alerta "Funcionalidad en desarrollo" en HTML

---
+++ js/products.js
@@ -0,0 +1,25 @@
+const DATA_URL = "https://japceibal.github.io/emercado-api/cats_products/101.json"
+const container = document.getElementById("cars-container");
+
+function createProducts(dataArray) {
+    for (const item of dataArray) {
+        container.innerHTML += `<div class="product-container">
+        <img class="product-img img-thumbnail" src="${item.image}">
+        <div class="product-text">
+             <h1 class="product-title">${item.name} - ${item.currency} ${item.cost}</h1>
+             <p class="product-description">${item.description}</p> 
+        </div>
+        <p class="product-count">${item.soldCount} vendidos</p>
+    </div>`
+    }
+}
+
+fetch(DATA_URL)
+.then(response => response.json())
+.then(data => {
+    console.log(data);
+    createProducts(data.products);
+})
+.catch(error => {
+    console.error("Error en el fetch: ", error);
+})
