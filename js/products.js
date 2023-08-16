86b957985027070d958462a06c3286760dec5e63
EusebioLujan <narboleta@gmail.com>
2023-08-16 14:33:14 -0300

    add href

--- js/products.js
+++ js/products.js
@@ -1,24 +1,24 @@
-const DATA_URL = "https://japceibal.github.io/emercado-api/cats_products/101.json"
+const DATA_URL = "https://japceibal.github.io/emercado-api/cats_products/101.json";
 const container = document.getElementById("cars-container");
 
 function createProducts(dataArray) {
-    for (const item of dataArray) {
-        container.innerHTML += `<div class="product-container">
+  for (const item of dataArray) {
+    container.innerHTML += `<a href="product-info.html "style="text-decoration: none; color: #7887a1;"><div class="product-container">
         <img class="product-img img-thumbnail" src="${item.image}">
         <div class="product-text">
              <h1 class="product-title">${item.name} - ${item.currency} ${item.cost}</h1>
              <p class="product-description">${item.description}</p> 
         </div>
         <p class="product-count">${item.soldCount} vendidos</p>
-    </div>`
-    }
+        
+    </div></a>`;
+  }
 }
 fetch(DATA_URL)
-.then(response => response.json())
-.then(data => {
-    console.log(data);
+  .then((response) => response.json())
+  .then((data) => {
     createProducts(data.products);
-})
-.catch(error => {
+  })
+  .catch((error) => {
     console.error("Error en el fetch: ", error);
-})
+  });
