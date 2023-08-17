413a2c9a13fdd9424c14a8900c39820af554fd9a
EusebioLujan <narboleta@gmail.com>
2023-08-17 02:46:16 -0300

    pre

--- js/index.js
+++ js/index.js
@@ -1,15 +1,36 @@
-console.log(localStorage.getItem("loggedin"))
-document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById("autos").addEventListener("click", function() {
-        localStorage.setItem("catID", 101);
-        window.location = "products.html"
-    });
-    document.getElementById("juguetes").addEventListener("click", function() {
-        localStorage.setItem("catID", 102);
-        window.location = "products.html"
-    });
-    document.getElementById("muebles").addEventListener("click", function() {
-        localStorage.setItem("catID", 103);
-        window.location = "products.html"
-    });
+console.log(localStorage.getItem("loggedin"));
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("autos").addEventListener("click", function () {
+    localStorage.setItem("catID", 101);
+    window.location = "products.html";
+  });
+  document.getElementById("juguetes").addEventListener("click", function () {
+    localStorage.setItem("catID", 102);
+    window.location = "products.html";
+  });
+  document.getElementById("muebles").addEventListener("click", function () {
+    localStorage.setItem("catID", 103);
+    window.location = "products.html";
+  });
+  const navbarWelcome = document.getElementById("navbarWelcome");
+  const isLoggedIn = localStorage.getItem("loggedin");
+  const email = localStorage.getItem("email");
+  const loginLink = document.getElementById("loginLink");
+  const logoutLink = document.getElementById("logoutLink");
+
+  if (isLoggedIn === "true" && email) {
+      
+      navbarWelcome.innerHTML = `<a href="my-profile.html" class="nav-link">Bienvenido, ${email}</a>`;
+      loginLink.style.display = "none";
+      logoutLink.style.display = "inline";
+  } else {
+    navbarWelcome.innerHTML = "";
+      loginLink.style.display = "center";
+      logoutLink.style.display = "none";
+  }
+
+  logoutLink.addEventListener("click", function() {
+      localStorage.setItem("loggedin", "false");
+      window.location.href = "login.html";
+  });
 });
---
+++ js/register.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const registrationForm = document.getElementById("registrationForm");
+    
+    registrationForm.addEventListener("submit", function(event) {
+      event.preventDefault();
+        const firstName = document.getElementById("nombre").value;
+        const lastName = document.getElementById("apellido").value;
+        const email = document.getElementById("correo").value;
+        const password = document.getElementById("contraseña").value;
+        const user = {
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: password
+      };
+      const users = JSON.parse(localStorage.getItem("users")) || [];
+      users.push(user);
+      localStorage.setItem("users", JSON.stringify(users));
+      Swal.fire({
+        icon: 'success',
+        title: '¡Datos Registrados con Exito!',
+        text: 'Aguarde Mientras Re-direccionamos.',
+        timer: 3000,
+        confirmButtonText: 'Aceptar'
+        }).then(()=>{location.href='login.html'})
+    });
+  });
+  
