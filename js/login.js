b17f137f5a100de5f4596594dde3877d859d8912
EusebioLujan <narboleta@gmail.com>
2023-08-28 11:46:38 -0300

    add register

--- js/login.js
+++ js/login.js
@@ -1,28 +1,57 @@
 let submitButton = document.getElementById("buttonsubmit")
 //Función de Verificación de usuario
 function ConfirmLogin() {
-   localStorage.setItem('loggedin', 'true'); location.href='index.html'
-   console.log(localStorage)
+   const email = document.getElementById("email").value;
+   const password = document.getElementById("password").value;
+   // Obtener la lista de usuarios desde el localStorage
+   const users = JSON.parse(localStorage.getItem("users")) || [];
+   // Buscar el usuario en la lista por su email
+   const user = users.find(user => user.email === email);
+   if (user && user.password === password) {
+      localStorage.setItem('loggedin', 'true'); 
+/* DATA PARA UTILIZAR EN EL NAVBAR PARA MOSTRAR EL CORREO */
+      localStorage.setItem('email', email);
+
+      Swal.fire({
+         icon: 'success',
+         title: '¡Datos Correctos!',
+         text: 'Aguarde Mientras Re-direccionamos.',
+         timer: 3000,
+         confirmButtonText: 'Aceptar'
+      }).then(() => {
+         location.href = 'index.html';
+      });
+   } else {
+      Swal.fire({
+         icon: 'error',
+         title: '¡Credenciales Incorrectas!',
+         text: 'Por favor, verifique su email y contraseña.',
+         confirmButtonText: 'Aceptar'
+      });
+   }
+
 }
-//Funciones de Error
-function MailError() {
-   alert("Hay un error en tu mail");
-}
-function PasswordError() {
-   alert("Hay un error en tu contraseña");
-}
+
+
+
 //Función de Validación
 function comprobarPwd () {
    const mail = document.getElementById("email").value
    const password = document.getElementById("password").value
    const estructura = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-   
-   console.log(estructura.test(mail))
    if(!estructura.test(mail)){
-      return MailError()
+      return Swal.fire({
+         icon: 'error',
+         title: '¡Datos de Email Incorrectos!',
+         confirmButtonText: 'Aceptar'
+      });
    }
    else if (password.length < 6 ) {               
-       return PasswordError()
+       return Swal.fire({
+         icon: 'error',
+         title: '¡Datos de Contraseñas Incorrectos!',
+         confirmButtonText: 'Aceptar'
+      });
    }
    else {        
        return ConfirmLogin()
