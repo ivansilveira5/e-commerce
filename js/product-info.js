d6959b9cf0e9d6054bf4ba9e9d9125951715741e
EusebioLujan <narboleta@gmail.com>
2023-08-29 11:05:05 -0300

    fix camelCase

--- js/product-info.js
+++ js/product-info.js
@@ -1,13 +1,13 @@
-const id =localStorage.getItem("productID")
+const id = localStorage.getItem("productID");
 
-const PRODUCT_URL= `https://japceibal.github.io/emercado-api/products/${id}.json`
+const PRODUCT_URL = `https://japceibal.github.io/emercado-api/products/${id}.json`;
 
-async function pepe() {
+async function fetchData() {
   await fetch(PRODUCT_URL)
-   .then(response=>response.json())
-   .then(data=>{
-       console.log(data);
-   })
-   .catch("error")
-} 
-console.log(pepe()); 
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch("error");
+}
+console.log(fetchData());
--- js/products.js
+++ js/products.js
@@ -1,38 +1,38 @@
-const catNumber = localStorage.getItem("catID")
-const DATA_URL = `https://japceibal.github.io/emercado-api/cats_products/${catNumber}.json`
+const catNumber = localStorage.getItem("catID");
+const DATA_URL = `https://japceibal.github.io/emercado-api/cats_products/${catNumber}.json`;
 const container = document.getElementById("cars-container");
 const searchInput = document.getElementById("searchInput");
 let filterData;
 
 function createProducts(dataArray) {
-    container.innerHTML = "";
-    for (const item of dataArray) {
-        container.innerHTML += `<div class="product-container">
+  container.innerHTML = "";
+  for (const item of dataArray) {
+    container.innerHTML += `<div class="product-container">
         <img class="product-img img-thumbnail" src="${item.image}">
         <div class="product-text">
              <h1 class="product-title">${item.name} - ${item.currency} ${item.cost}</h1>
              <p class="product-description">${item.description}</p> 
         </div>
         <p class="product-count">${item.soldCount} vendidos</p>
-    </div>`
-    }
+    </div>`;
+  }
 }
 fetch(DATA_URL)
-.then(response => response.json())
-.then(data => {
+  .then((response) => response.json())
+  .then((data) => {
     filterData = data.products;
     createProducts(data.products);
-})
-.catch(error => {
+  })
+  .catch((error) => {
     console.error("Error en el fetch: ", error);
-})
-function initialize(){
-
-searchInput.addEventListener("input", () => {
+  });
+function initialize() {
+  searchInput.addEventListener("input", () => {
     const searchTerm = searchInput.value.toLowerCase();
-    const filteredProducts = filterData.filter((product) => product?.name?.toLowerCase().includes(searchTerm));
+    const filteredProducts = filterData.filter((product) =>
+      product?.name?.toLowerCase().includes(searchTerm)
+    );
     createProducts(filteredProducts);
   });
-
 }
-initialize()
+initialize();
