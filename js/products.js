2d32a3f7b7048b5cf0144ea0c7228b62c0b62b2c
thiagoleoncino <thiagoleoncino@gmail.com>
2023-08-31 10:33:00 -0300

    Filtros Products Js

--- js/products.js
+++ js/products.js
@@ -17,6 +17,7 @@ function createProducts(dataArray) {
     </div>`;
   }
 }
+
 fetch(DATA_URL)
   .then((response) => response.json())
   .then((data) => {
@@ -34,5 +35,56 @@ function initialize() {
     );
     createProducts(filteredProducts);
   });
+  sortDesc.addEventListener("click", () => {
+    createProducts([...filterData].sort((a, b) => b.cost - a.cost));
+  })
+  sortAsc.addEventListener("click", () => {
+    createProducts([...filterData].sort((a, b) => a.cost - b.cost));
+  })
+  sortByCount.addEventListener("click", () => {
+    createProducts([...filterData].sort((a, b) => b.soldCount - a.soldCount));
+  })
 }
 initialize();
+
+document.getElementById("clearRangeFilter").addEventListener("click", function(){
+    document.getElementById("rangeFilterCountMin").value = "";
+    document.getElementById("rangeFilterCountMax").value = "";
+
+    minCount = undefined;
+    maxCount = undefined;
+});
+
+document.getElementById("rangeFilterCount").addEventListener("click", function(){
+    //Obtengo el mínimo y máximo de los intervalos para filtrar por cantidad
+    //de productos por categoría.
+    minCount = document.getElementById("rangeFilterCountMin").value;
+    maxCount = document.getElementById("rangeFilterCountMax").value;
+
+    if ((minCount != undefined) && (minCount != "") && (parseInt(minCount)) >= 0){
+        minCount = parseInt(minCount);
+    }
+    else{
+        minCount = undefined;
+    }
+
+    if ((maxCount != undefined) && (maxCount != "") && (parseInt(maxCount)) >= 0){
+        maxCount = parseInt(maxCount);
+    }
+    else{
+        maxCount = undefined;
+    }
+    
+    function filterProductsPrice()
+    {
+        let filterPrice = filterData
+        if (minCount !== undefined || maxCount !== undefined)
+        {
+            filterPrice = filterData.filter((product)=> product.cost <= maxCount && product.cost >= minCount)
+        }
+        createProducts(filterPrice);
+    }
+
+    filterProductsPrice();
+
+});
