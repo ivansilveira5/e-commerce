d86491564e198bba37c822b2a6aafef5293387a0
Che-Pibe27 <ivannsilveiraa@gmail.com>
2023-09-06 22:58:28 -0300

    Redirect y build de product info

--- js/product-info.js
+++ js/product-info.js
@@ -1,4 +1,5 @@
 const id = localStorage.getItem("productID");
+const container = document.getElementById("product-container")
 
 const PRODUCT_URL = `https://japceibal.github.io/emercado-api/products/${id}.json`;
 
@@ -7,7 +8,21 @@ async function fetchData() {
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
+      createProducts(data);
     })
     .catch("error");
 }
 console.log(fetchData());
+
+function createProducts(Obj) {
+    container.innerHTML +=     
+    `<p>Nombre ${Obj.name} </p>
+    <p>Descripción ${Obj.description}</p>
+    <p>Categoría ${Obj.category}</p>
+    <p>Cantidad de vendidos ${Obj.soldCount} </p>
+    <p>Imágenes ilustrativas</p>`
+    Obj.images.map((image)=> {
+      container.innerHTML += `<img class="product-img img-thumbnail" src=${image}>`}
+    );    
+}
+
